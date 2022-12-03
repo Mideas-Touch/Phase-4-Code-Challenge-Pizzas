13d9a5f4eb2feb6332eebf9c373e84e951739c73
fliblip <wickynjy@gmail.com>
2022-12-03 11:00:09 +0300

    Added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'restaurant/pizza'
+  get 'restaurant/restaurant_pizza'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
