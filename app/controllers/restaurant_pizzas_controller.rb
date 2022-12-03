2d9d2d5b78852a6b7987543d0d2b7a63dc61f686
fliblip <wickynjy@gmail.com>
2022-12-03 12:48:45 +0300

    Added serializers

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,16 +1,16 @@
 class RestaurantPizzasController < ApplicationController
 
     def index
-        render json: RestaurantPizza.all, status: :ok
+        render json: RestaurantPizza.all
     end
 
     def show
         restaurant_pizza = RestaurantPizza.find_by(id:params[:id])
         if restaurant_pizza
-            render json:
-            restaurant_pizza
+            render json: restaurant_pizza
         else
             render json: {error: "Restaurant Pizza not found"}, status: :not_found
         end
+
     end
 end
