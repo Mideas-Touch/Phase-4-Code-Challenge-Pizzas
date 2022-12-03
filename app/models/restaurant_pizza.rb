e83ab54be5d84fb13baf67031bec5c02a1808e50
fliblip <wickynjy@gmail.com>
2022-12-03 13:18:12 +0300

    Added price &  range validation

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -2,5 +2,12 @@ class RestaurantPizza < ApplicationRecord
     belongs_to :pizza
     belongs_to :restaurant
 
-    validates :price, length: { in: 1..30}
+    validates :price, presence: true
+    validate :price_range
+
+    def price_range
+        if !(price < 30 && price > 0)
+            errors.add(:price, "Price should be between 0 and 30")
+        end
+    end
 end 
