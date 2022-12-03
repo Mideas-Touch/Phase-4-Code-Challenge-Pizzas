02a65ce47901e76ecd00193c2d956d43373eadb3
fliblip <wickynjy@gmail.com>
2022-12-03 11:23:45 +0300

    Added rails routes

--- app/controllers/pizza_controller.rb
+++ app/controllers/pizza_controller.rb
@@ -1,2 +1,3 @@
 class PizzaController < ApplicationController
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  get 'restaurant/pizza'
-  get 'restaurant/restaurant_pizza'
+  resources :restaurants
+  resources :pizzas
+  resources :restaurant_pizzas
+
+
+  # get 'restaurant/pizza'
+  # get 'restaurant/restaurant_pizza'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
 end
